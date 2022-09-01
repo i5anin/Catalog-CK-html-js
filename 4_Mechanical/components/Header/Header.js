ca24dce428905a3d39fb1738e0ecdabd384c0cf6
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-09-01 14:59:25 +0300

    init

---
+++ 4_Mechanical/components/Header/Header.js
@@ -0,0 +1,34 @@
+class Header {
+  handlerOpenShoppingPage() {
+    shoppingPage.render();
+  }
+
+  render(count) {
+    const html = `
+
+          <div class="header-container">
+            <div div class="header-counter" >
+            <a class="header-link" href="https://salesk.ru/1_Power/">Электродвигатели</a> 
+            <a class="header-link" href="https://salesk.ru/2_Ventilation_gas/">Вентиляционные и газовые</a>
+            <a class="header-link" href="https://salesk.ru/3_Electronics/">Электроника</a>
+            <a class="header-link" href="https://salesk.ru/4_Mechanical/">Механообработка</a>
+            </div>
+          </div>
+
+            <div class="header-container2">
+                <div >
+                    <strong><a class="number" href="tel:+79056132557" rel="nofollow">+7(905)-613-25-57</a></strong>
+                </div>  
+
+                <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();" >
+                    🔥 Корзина: ${count}
+                </div>
+            </div>
+
+            <a href="#" title="Вернуться к началу" class="topbutton">^</a>
+        `;
+    ROOT_HEADER.innerHTML = html;
+  }
+}
+
+const headerPage = new Header();
