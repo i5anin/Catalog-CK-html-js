e4ba85d5ba8d5110ed1094f2e8da3f9531bc76d7
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-09-02 09:57:11 +0300

    style

--- 3_Electronics/components/Header/Header.js
+++ 3_Electronics/components/Header/Header.js
@@ -7,17 +7,17 @@ class Header {
     const html = `
 
           <div class="header-container">
-            <div div class="header-counter" >
-            <a class="header-link" href="https://salesk.ru/1_Power/">Электродвигатели</a> 
-            <a class="header-link" href="https://salesk.ru/2_Ventilation_gas/">Вентиляционные и газовые</a>
-            <a class="header-link" href="https://salesk.ru/3_Electronics/">Электроника</a>
-            <a class="header-link" href="https://salesk.ru/4_Mechanical/">Механообработка</a>
+            <div div class="header-counter">
+              <a href="https://salesk.ru/1_Power/" class="btn">Электродвигатели</a>
+              <a href="https://salesk.ru/2_Ventilation_gas/" class="btn">Вентиляционные и газовые</a>    
+              <a href="https://salesk.ru/3_Electronics/" class="btn">Электроника</a>    
+              <a href="https://salesk.ru/4_Mechanical/" class="btn">Механообработка</a>    
             </div>
           </div>
 
             <div class="header-container2">
                 <div >
-                    <strong><a class="number" href="tel:+79056132557" rel="nofollow">+7(905)-613-25-57</a></strong>
+                    <strong><a class="btn-number number" href="tel:+79056132557" rel="nofollow">+7(905)-613-25-57</a></strong>
                 </div>  
 
                 <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();" >
